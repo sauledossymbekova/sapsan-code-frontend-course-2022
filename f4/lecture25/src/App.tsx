0f9476c346aee2956f7827ef754a313a192cdaaa
Saule <SDossymbekova@one.kz>
2023-02-17 16:20:04 +0600

    f4: lecture25

---
+++ f4/lecture25/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { GreetingComponent } from "./GreetingComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <GreetingComponent />
+    </div>
+  );
+}
+
+export default App;
